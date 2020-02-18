0d0f5b823638072f157c0e31d4f83d1d976b2ac4
muhdaniboyrendi <muhdaniboyrendi@gmail.com>
2020-02-18 11:32:38 +0700

    Menambahkan file

---
+++ application/controllers/Auth.php
@@ -0,0 +1,37 @@
+<?php
+    defined('BASEPATH') OR exit('No direct script access allowed');
+
+    class Auth extends CI_Controller {
+        public function __construct(){
+            parent::__construct();
+            $this->load->library('form_validation');
+        }
+        public function index(){
+            $this->form_validation->set_rules('nama', 'Name', 'required|trim');
+            $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email');
+            $this->form_validation->set_rules('gender', 'Gender', 'required');
+            $this->form_validation->set_rules('city', 'City', 'required');
+            $this->form_validation->set_rules('status', 'Status', 'required');
+
+            if ($this->form_validation->run() == false){
+                $data['title'] = ' User Registration';
+                $this->load->view('templates/auth_header', $data);
+                $this->load->view('auth/registration.php');
+                $this->load->view('templates/auth_footer');
+            }else{
+                $data = [
+                    'nama' => $this->input->post('nama'),
+                    'email' => $this->input->post('email'),
+                    'gender' => $this->input->post('gender'),
+                    'city' => $this->input->post('city'),
+                    'status' => $this->input->post('status'),
+                ];
+                $this->db->insert('users', $data);
+                $this->session->set_flashdata('messege', '<div class="alert alert-success" role="alert">
+                                                            Your Account has been Succesfully Created
+                                                            </div>');
+                redirect('auth');
+            }
+        }
+    }
+?>
---
+++ application/views/auth/registration.php
@@ -0,0 +1,86 @@
+<div class="signupbox">
+    <div class="header">
+        <h2>Signup</h2>
+    </div>
+    <form action="<?= base_url('auth/index'); ?>" method="post">
+        <div class="user">
+            <!-- input text -->
+            <div class="top">
+                <label>Name </label><br>
+                <input name="nama" id="nama" type="text" placeholder="Enter Your Name" value="<?= set_value('name'); ?>">
+                <br>
+                <font color="red">
+                    <?= form_error('nama', '<small>', '</small>'); ?>
+                </font>
+                <br>
+                <label>Email </label><br>
+                <input name="email" id="email" type="text" placeholder="Enter Your Email" value="<?= set_value('email'); ?>">
+                <br>
+                <font color="red">
+                    <?= form_error('email', '<small>', '</small>'); ?>
+                </font>
+            </div>
+            <!-- end input text -->
+            <br>
+            <!-- gender -->
+            <div class="gender">
+                <p>Gender :</p>
+                <input name="gender" id="gender" type="radio" value="Male">
+                <label>Male</label>
+                <input name="gender" id="gender" type="radio" value="Female">
+                <label>Female</label>
+                <br>
+                <font color="red">
+                    <?= form_error('gender', '<small>', '</small>'); ?>
+                </font>
+            </div>
+            <!-- end gender -->
+            <br>
+            <!-- city -->
+            <div class="bottom">
+                <p>City :</p>
+                <select name="city" id="city">
+                    <option value=""></option>
+                    <option value="Solo">Solo</option>
+                    <option value="Karanganyar">Karanganyar</option>
+                    <option value="Sukoharjo">Sukoharjo</option>
+                    <option value="Sragen">Sragen</option>
+                </select>
+                <br>
+                <font color="red">
+                    <?= form_error('city', '<small>', '</small>'); ?>
+                </font>
+            </div>
+            <!-- end city -->
+            <br>
+            <!-- status -->
+            <div class="status">
+                <p>Status :</p>
+                <input type="checkbox" name="status" id="status" value="Pelajar">
+                <label>Pelajar</label>
+                <input type="checkbox" name="status" id="status" Value="Kerja">
+                <label>Kerja</label>
+                <input type="checkbox" name="status" id="status" value="Kuliah">
+                <label>Kuliah</label>
+                <br>
+                <font color="red">
+                    <?= form_error('status', '<small>', '</small>'); ?>
+                </font>
+            </div>
+            <!-- end status -->
+            <br>
+        </div>
+        <!-- submit -->
+        <div class="sub">
+            <input type="submit" name="submit" value="Submit" />
+        </div>
+        <!-- end submit -->
+    </form>
+</div>
+<!-- alert saat data yang diinputkan sudah masuk ke database -->
+<?php
+echo "<script>
+        alert('Your Account has been Successfully Created');
+    </script>"
+?>
+<!-- end alert -->
